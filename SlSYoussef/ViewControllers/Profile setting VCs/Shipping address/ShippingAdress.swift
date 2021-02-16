cd7db3e92f5873834e568279dd6c6e5ee847b7d5
Hady Helal <helalhady16@gmail.com>
2021-02-16 15:29:22 +0200

    last mofigy shipping address

--- SlSYoussef/ViewControllers/Profile setting VCs/Shipping address/ShippingAdress.swift
+++ SlSYoussef/ViewControllers/Profile setting VCs/Shipping address/ShippingAdress.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 import UITextView_Placeholder
+import Firebase
+import FirebaseDatabase
 class ShippingAdress: UIViewController {
-
+    
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var saveBtn: UIButton!
+    let dp = Firestore.firestore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,7 +32,48 @@ class ShippingAdress: UIViewController {
         
     }
     
+    var data = [String:String]()
+    
+    
+    
+    @IBAction func save(_ sender: UIButton) {
+        self.showLoadingView()
+        if currentUserIsValid() {
+            if let txt = textView.text , txt.trimmingCharacters(in: .whitespacesAndNewlines) != "" {
+                data[User.shippingAddress] = txt
+            }
+            dp.collection("user").document(currentlyUserUid!).setData(data, merge: true) { (err) in
+                if err != nil {
+                    self.hideLoadingView()
+                    self.showAlert(title: "Error", message: err!.localizedDescription)
+                }
+                else {
+                    self.hideLoadingView()
+                    self.showAlert(title: "Success", message: "You successfully updated your data")
+                }
+            }
+        }
+    }
+    
+    var currentlyUserUid : String?
+    func currentUserIsValid() -> Bool {
+        var isOnline : Bool = false
+        Auth.auth().addStateDidChangeListener { (auth, user) in
+            if let user = user {
+                self.currentlyUserUid = user.uid
+                isOnline = true
+            }
+            else {
+                self.showAlert(title: "Error", message: "Something wrong please check your internet connection and try again")
+                isOnline = false
+            }
+            
+        }
+        return isOnline
+    }
+    
     @IBAction func closeBtn(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
     }
     
 }
